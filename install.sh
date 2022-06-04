e36849e16879674d614f9f7cf17e5df94bd32975
c0ffeeAddict <105886757+c0ffeeAddict0veride@users.noreply.github.com>
2022-06-04 17:55:37 +0100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+echo "Make sure your rooted and have Go installed (https://www.youtube.com/watch?v=dr2Wo7qETZ8)"
+
+sleep 1
+git clone https://github.com/s0md3v/XSStrike.git
+cd XSStrike
+pip3 install -r requirements.txt
+cd 
+
+sleep 1 
+
+clear 
+
+sudo apt install xsser
+sudo apt install ffuf
+sleep 1 
+go get github.com/tomnomnom/waybackurls
+cp /root/go/bin/waybackurls /usr/local/go/bin/
+
+
+echo "Done"
+sleep 1
+clear 
+echo "ignore this https://pastebin.com/ZbmGyXRj "
+done
+
